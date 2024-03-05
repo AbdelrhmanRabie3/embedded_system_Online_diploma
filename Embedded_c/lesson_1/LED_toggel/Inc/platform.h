56c974f2db67dd3c82efc9482a1b503e84d0e235
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2024-03-06 00:27:40 +0200

    toggel LED

---
+++ Embedded_c/lesson_1/LED_toggel/Inc/platform.h
@@ -0,0 +1,40 @@
+
+/*Author      : Abdelrhman_Rabie
+ Version     :01
+ Copyright   : Your copyright notice
+ Description : Platform types for st30
+ ============================================================================
+ */
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H
+
+typedef unsigned char 					boolen;
+typedef unsigned char					uint8;
+typedef unsigned short					uint16;
+typedef unsigned long 		    		uint32;
+typedef unsigned long long 				uint64;
+typedef unsigned long					uint8_least;
+typedef unsigned long					uint16_least;
+typedef unsigned long					uint32_least;
+typedef volatile unsigned char 			vuint8;
+typedef volatile unsigned short 		vuint16;
+typedef volatile unsigned long 		    vuint32;
+typedef volatile unsigned long long 	vuint64;
+		
+typedef signed char 					sint8;
+typedef signed short 					sint16;
+typedef signed long 		    		sint32;
+typedef signed long long 				sint64;
+typedef signed long						sint8_least;
+typedef signed long						sint16_least;
+typedef signed long						sint32_least;
+typedef volatile signed char 			vsint8;
+typedef volatile signed short 			vsint16;
+typedef volatile signed long 		    vsint32;
+typedef volatile signed long long 		vsint64;
+
+typedef float 							float32;
+typedef double 							float64;
+
+
+#endif
