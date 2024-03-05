d9097aa32f74d8235e5c2b25633aa50ba6982b18
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2024-03-06 00:30:08 +0200

    static_lib

---
+++ Embedded_c/lesson_1/static lib/can.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "can.h"
+
+extern void CAN_init()
+{
+	printf("CAN_INIT>>>>>\n");
+}
---
+++ Embedded_c/lesson_1/static lib/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "can.h"
+
+int main()
+{
+	CAN_init();
+	return 0;
+}
